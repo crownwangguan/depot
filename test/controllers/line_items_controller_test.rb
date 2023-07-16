adcf79366595818d1145f36d6c6a613f187b2734
crownwangguan <crown.wg@outlook.com>
2023-07-16 15:00:15 -0700

    ajax partial render

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -51,4 +51,12 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to cart_url(session[:cart_id])
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
+    end
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/, @response.body
+  end
 end
