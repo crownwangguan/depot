d9859d7a2e6246ebc2a96004f068f95ae16cc4e7
crownwangguan <crown.wg@outlook.com>
2023-07-25 22:17:28 -0700

    use turbo:load event listener

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PayTypeSelector from './pay_type_selector'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbo:load', () => {
     let element = document.getElementById("pay-type-component");
     ReactDOM.render(<PayTypeSelector />, element);
 });
