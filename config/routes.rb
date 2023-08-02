f47bfec866d108f2f94f2aa5eb4d4ae91b48718b
crownwangguan <crown.wg@outlook.com>
2023-08-02 15:54:00 -0700

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   resources :orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :line_items
